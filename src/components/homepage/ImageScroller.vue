f28025e15b78f2d86b214eb164ab77e64e177906
Jeremias Gruenauer <jeremiasgruenauers@gmail.com>
2023-07-11 00:17:09 +0200

    timer info icon

--- src/components/homepage/ImageScroller.vue
+++ src/components/homepage/ImageScroller.vue
@@ -19,74 +19,74 @@ export default {
             translateX: 0,
             hovered: false,
             hoverSpeed: 2,
-      slowSpeed: 1,
-      animationId: null
-    };
-  },
-  computed: {
-    extendedImages() {
-      // Duplicate the images array to create a continuous loop
-      // future usability may require to check if two images are enough!
-      return [...this.images, ...this.images];
-    }
-  },
-  methods: {
-    
-    animate() {
-      this.translateX -= this.hovered ? this.hoverSpeed : this.slowSpeed;
-
-      const containerWidth = this.$refs.images.offsetWidth;
-
-      // Check if scrolled beyond the total width of the images
-      if (Math.abs(this.translateX) >= containerWidth/2) {
-        this.translateX = 0;
-      }
-
-      this.animationId = requestAnimationFrame(this.animate);
+            slowSpeed: 1,
+            animationId: null
+        };
     },
-    handleMouseOver() {
-      this.hovered = true;
+    computed: {
+        extendedImages() {
+            // Duplicate the images array to create a continuous loop
+            // future usability may require to check if two images are enough!
+            return [...this.images, ...this.images];
+        }
     },
-    handleMouseLeave() {
-      this.hovered = false;
+    methods: {
+
+        animate() {
+            this.translateX -= this.hovered ? this.hoverSpeed : this.slowSpeed;
+
+            const containerWidth = this.$refs.images.offsetWidth;
+
+            // Check if scrolled beyond the total width of the images
+            if (Math.abs(this.translateX) >= containerWidth / 2) {
+                this.translateX = 0;
+            }
+
+            this.animationId = requestAnimationFrame(this.animate);
+        },
+        handleMouseOver() {
+            this.hovered = true;
+        },
+        handleMouseLeave() {
+            this.hovered = false;
+        }
+    },
+    mounted() {
+        this.animationId = requestAnimationFrame(this.animate);
+    },
+    beforeUnmount() {
+        cancelAnimationFrame(this.animationId);
     }
-  },
-  mounted() {
-    this.animationId = requestAnimationFrame(this.animate);
-  },
-  beforeUnmount() {
-    cancelAnimationFrame(this.animationId);
-  }
 };
 </script>
 
 <style>
 .image-scroll {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  overflow: hidden;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
 }
 
 .scroll-container {
-  white-space: nowrap;
-  height: 100%;
+    white-space: nowrap;
+    height: 100%;
 }
 
 .scroll-container.hovered {
-  transition: transform 0.2s ease-in-out;
+    transition: transform 0.2s ease-in-out;
 }
 
 .images {
-  display: inline-block;
-  height: 100%;
+    display: inline-block;
+    height: 100%;
 }
 
 img {
-  display: inline-block;
-  height: 100%;
+    display: inline-block;
+    height: 100%;
 
 }
 </style>
